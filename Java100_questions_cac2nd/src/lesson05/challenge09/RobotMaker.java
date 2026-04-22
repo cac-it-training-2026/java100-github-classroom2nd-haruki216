ac1949959acba34dccdadf40fc6fc69e2a00846e
hmiyauch <hmiyauch@C010423641.zipangu.cac.co.jp>
2026-04-22 16:25:52 +0900

    lesson1,2,3,5終了 オブジェクトをしたかったので4はいったん飛ばし中

--- Java100_questions_cac2nd/src/lesson05/challenge09/RobotMaker.java
+++ Java100_questions_cac2nd/src/lesson05/challenge09/RobotMaker.java
@@ -51,7 +51,6 @@ package lesson05.challenge09;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 //ここに問題8で作成したクラス(変更なし)を記述してください。
 
@@ -60,65 +59,125 @@ import java.util.Arrays;
 //メソッド名：clearTable(引数int[] ingredients
 //戻り値void、渡された配列を0でクリアする)
 
+class Robot {
+	int energy;
+	int water;
+	String name;
+
+	void pumpWater() {
+		System.out.println("水を" + water + "リットル出します。\n");
+	}
+
+	void randomWater() {
+		water = (int) (Math.random() * 10) % 9 + 1;
+	}
+
+	void setWater(int water) {
+		this.water = water;
+	}
+
+	void makeOmelet(int eggNum, int butterNum) {
+		int bestOmeletNum1 = eggNum / 2;
+		int bestOmeletNum2 = butterNum / 5;
+
+		if (bestOmeletNum1 > bestOmeletNum2) {
+			System.out.println("\n" + bestOmeletNum2 + "人分のオムレツを作成しました。\n");
+		} else {
+			System.out.println("\n" + bestOmeletNum1 + "人分のオムレツを作成しました。\n");
+		}
+	}
+
+	int getWater() {
+
+		return water;
+	}
+
+	void makecookie(int flourNum, int sugarNum, int eggNum, int butterNum) {
+		if (flourNum >= 170 && sugarNum >= 50 && eggNum >= 1 && butterNum >= 80) {
+			System.out.println("クッキーができました");
+		} else {
+			if (eggNum >= 2 && butterNum >= 5) {
+				System.out.println("オムレツができました");
+			} else if (eggNum > 0 && butterNum < 5) {
+				System.out.println("ゆで卵ができました");
+			} else {
+				System.out.println("何もできませんでした");
+			}
+		}
+
+	}
+
+	int[] ingredients = new int[4];
+
+	//	void setingredients(int[] ingredients) {
+	//		this.ingredients = ingredients;
+	//	}
+
+	void clearRobot(int[] ingredients) {
+
+		for (int i = 0; i < ingredients.length; i++) {
+			ingredients[i] = ingredients[i] - ingredients[i];
+		}
+	}
+}
+
 public class RobotMaker {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.println("Rさん：");
-        System.out.println("あとかたづけをしてくれるロボットも欲しいところですね。\n");
-        System.out.println("G博士：");
-        System.out.println("そうれはもう作ってあるぞ。\n");
-        System.out.println("Rさん：");
-        System.out.println("えっ！どうやって使うんですか？\n");
-        System.out.println("G博士：");
-        System.out.println("今まで使ってきた材料をまとめて、料理と一緒に渡すときれいにしてくれるんじゃ。\n");
-        System.out.println("Rさん：");
-        System.out.println("早速やってみます。\n");
+		System.out.println("Rさん：");
+		System.out.println("あとかたづけをしてくれるロボットも欲しいところですね。\n");
+		System.out.println("G博士：");
+		System.out.println("そうれはもう作ってあるぞ。\n");
+		System.out.println("Rさん：");
+		System.out.println("えっ！どうやって使うんですか？\n");
+		System.out.println("G博士：");
+		System.out.println("今まで使ってきた材料をまとめて、料理と一緒に渡すときれいにしてくれるんじゃ。\n");
+		System.out.println("Rさん：");
+		System.out.println("早速やってみます。\n");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("小麦粉の量を入力してください（グラム）＞");
-        String flourNumStr = br.readLine();
-        int flourNum = Integer.parseInt(flourNumStr);
+		System.out.print("小麦粉の量を入力してください（グラム）＞");
+		String flourNumStr = br.readLine();
+		int flourNum = Integer.parseInt(flourNumStr);
 
-        System.out.print("\n砂糖の量を入力してください（グラム）＞");
-        String sugarNumStr = br.readLine();
-        int sugarNum = Integer.parseInt(sugarNumStr);
+		System.out.print("\n砂糖の量を入力してください（グラム）＞");
+		String sugarNumStr = br.readLine();
+		int sugarNum = Integer.parseInt(sugarNumStr);
 
-        System.out.print("\n卵の個数を入力してください＞");
-        String eggNumStr = br.readLine();
-        int eggNum = Integer.parseInt(eggNumStr);
+		System.out.print("\n卵の個数を入力してください＞");
+		String eggNumStr = br.readLine();
+		int eggNum = Integer.parseInt(eggNumStr);
 
-        System.out.print("\nバターの量を入力してください（グラム）＞");
-        String butterNumStr = br.readLine();
-        int butterNum = Integer.parseInt(butterNumStr);
+		System.out.print("\nバターの量を入力してください（グラム）＞");
+		String butterNumStr = br.readLine();
+		int butterNum = Integer.parseInt(butterNumStr);
 
+		//ここでRobotクラスのインスタンスを作り、
+		//（インスタンス名はrobot）
+		//makeEggDishesを実行する。
+		//標準出力でメニューを表示する。
 
-        //ここでRobotクラスのインスタンスを作り、
-        //（インスタンス名はrobot）
-        //makeEggDishesを実行する。
-        //標準出力でメニューを表示する。
+		System.out.println("\nあとかたづけをします。\n");
+		int[] ingredients = new int[4];
+		ingredients[0] = flourNum;
+		ingredients[1] = sugarNum;
+		ingredients[2] = eggNum;
+		ingredients[3] = butterNum;
 
+		//ここでClearRobotクラスのインスタンスを作り、
+		//（インスタンス名はclearRobot）
+		//clearTableを実行する。
+		Robot robot = new Robot();
+		robot.clearRobot(ingredients);
 
-        System.out.println("\nあとかたづけをします。\n");
-        int[] ingredients = new int[4];
-        ingredients[0] = flourNum;
-        ingredients[1] = sugarNum;
-        ingredients[2] = eggNum;
-        ingredients[3] = butterNum;
+		System.out.println("小麦粉  ：" + ingredients[0] + "g");
+		System.out.println("砂糖    ：" + ingredients[1] + "g");
+		System.out.println("卵      ：" + ingredients[2] + "個");
+		System.out.println("バター  ：" + ingredients[3] + "g");
 
-
-        //ここでClearRobotクラスのインスタンスを作り、
-        //（インスタンス名はclearRobot）
-        //clearTableを実行する。
-
-
-        System.out.println("小麦粉  ：" + ingredients[0] + "g");
-        System.out.println("砂糖    ：" + ingredients[1] + "g");
-        System.out.println("卵      ：" + ingredients[2] + "個");
-        System.out.println("バター  ：" + ingredients[3] + "g");
-
-        System.out.println("\nきれいになりました。");
-    }
+		System.out.println("\nきれいになり)ました。");
+	}
 
 }
